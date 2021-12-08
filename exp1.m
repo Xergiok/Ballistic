b3913e46b1a363ae8742965d4c78c4d51bef8cb0
Xergiok <60292966+Xergiok@users.noreply.github.com>
2021-12-08 21:39:14 +0300

    just f
    problems with resistance

---
+++ exp1.m
@@ -0,0 +1,20 @@
+clear; close all; clc;
+format short;
+
+% opts = struct('WindowStyle','non-modal',... 
+%               'Interpreter','tex');
+% f = warndlg('\color{blue} Pressing ACCEPT clears memory',...
+%              'Memory Warning', opts);
+
+% opts.Interpreter = 'tex';
+% % Include the desired Default answer
+% opts.Default = 'ok';
+% % Use the TeX interpreter to format the question
+% quest = {   'afasdf', ...
+%             'asdgsd'    };
+% answer = questdlg(quest,'Инфо',...
+%                   'ok',opts);
+
+a = zeros(4, 1)
+b = ones(3, 1)
+size(a , 2)
